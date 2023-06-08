fa6ef257108b6bd1fbdfb8ca961feb9dbc922adc
Adrian G <92943726+Hifilo@users.noreply.github.com>
2023-06-08 10:05:05 -0400

    initial repo

---
+++ README.md
@@ -0,0 +1,25 @@
+# constructor-helper README
+
+## Features
+
+My own extension to improve class writing. just highlight the parameters inside the constructor, run the extension, and have it add this.parameters inside the {}
+
+## Requirements
+
+None
+
+## Extension Settings
+
+None at the moment
+
+## Known Issues
+
+None
+
+## Release Notes
+
+simple extension
+
+### 1.0.0
+
+Initial release
---
+++ out/extension.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = void 0;
+const vscode = require("vscode");
+function activate(context) {
+    let disposable = vscode.commands.registerCommand('extension.constructorHelper', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            const selection = editor.selection;
+            const text = document.getText(selection);
+            const params = text.split(',');
+            const refactoredParams = params.map((param) => {
+                const trimmedParam = param.trim();
+                return `this.${trimmedParam} = ${trimmedParam};`;
+            });
+            const refactoredText = refactoredParams.join('\n');
+            editor.edit(editBuilder => {
+                const constructorPosition = selection.active;
+                const constructorLine = document.lineAt(constructorPosition.line);
+                // Check if the constructor is already defined
+                if (constructorLine.text.trim().startsWith('constructor')) {
+                    const constructorRange = constructorLine.range;
+                    const openingBraceLine = document.lineAt(constructorRange.end.line + 1);
+                    const startPosition = new vscode.Position(openingBraceLine.lineNumber, 0);
+                    const newConstructorText = `\n${refactoredText}\n`;
+                    editBuilder.insert(startPosition, newConstructorText);
+                }
+            });
+        }
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+//# sourceMappingURL=extension.js.map
